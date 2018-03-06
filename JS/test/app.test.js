e21caf8a79bda656ed43c01c8b27c756d0cff361
Steve Hughes <yosser@gmail.com>
2018-03-06 15:36:47 +0000

    utest for removal of punctuation

--- JS/test/app.test.js
+++ JS/test/app.test.js
@@ -36,35 +36,9 @@ test('it should figure out what to do with white space', () => {
   expect(app.isPalindrome('is this a palindrome')).toBe(false);
 })
 
+test('it should figure out what to do with punctuation', () => {
+  expect(app.isPalindrome('Was it a car or a cat I saw?')).toBe(true);
+  expect(app.isPalindrome('A man, a plan, a canal, Panama!')).toBe(true);
+  expect(app.isPalindrome('is this a palindrome?')).toBe(false);
+})
 
-
-test("it should print hello world", () => {
-  expect(app.helloWorld()).toBe("hello world");
-});
-
-test("It should print 1 as first number", () => {
-  expect(app.printNumbers()[0]).toBe(1);
-});
-
-test("The length of the returned array should be 100", () => {
-  expect(app.printNumbers().length).toBe(100);
-});
-
-test("It should print buzz instead of last number", () => {
-  expect(app.printNumbers().pop()).toBe("buzz");
-});
-
-test("It should print fizz for the first 3rd number", () => {
-  let allNumbers = app.printNumbers();
-  expect(allNumbers[2]).toBe("fizz");
-});
-
-test("It should print buzz for every 5th number", () => {
-  let allNumbers = app.printNumbers();
-  
-  for (let i = 0; i < allNumbers.length; i++) {
-    if (allNumbers[i] % 5 == 0) {
-      expect(allNumbers[i]).toBe("buzz");
-    }
-  }
-});
