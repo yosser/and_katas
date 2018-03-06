79991b7c5de91c13f40ebc2d1bd7ec5e1f36a623
Steve Hughes <yosser@gmail.com>
2018-03-06 15:41:52 +0000

    removed spurious code

--- JS/src/app.js
+++ JS/src/app.js
@@ -1,10 +1,5 @@
-function helloWorld() {
-  return "hello world";
-}
 
 module.exports = {
-  helloWorld: helloWorld,
-  printNumbers: printNumbers,
   isPalindrome: isPalindrome
 
 };
@@ -18,23 +13,5 @@ function isPalindrome(candidate){
 }
 
 
-
-function printNumbers() {
-  let numbers = [];
-
-  for(let i = 1; i <= 100; i++){
-    if (i % 3 === 0) {
-      numbers.push("fizz");
-    } 
-    else if (i % 5 === 0) {
-      numbers.push("buzz");
-    }
-    else {
-      numbers.push(i);
-    }
-  }
-
-  return numbers;
-}
 
 
