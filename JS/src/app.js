16cfc11ce8333bf5960823dd99f8bd8bf441e3a6
Steve Hughes <yosser@gmail.com>
2018-03-06 15:33:05 +0000

    code for removal of white space

--- JS/src/app.js
+++ JS/src/app.js
@@ -11,6 +11,7 @@ module.exports = {
 
 function isPalindrome(candidate){
   candidate = candidate.toLowerCase();
+  candidate = candidate.replace(/ /g, '');
   if (candidate==='') {
     return false;
   }
